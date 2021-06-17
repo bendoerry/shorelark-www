58dd116cf65418c8ae5d2c650f005335621dde24
Benjamin Doerry <ben.doerry@gmail.com>
2021-06-17 22:27:31 +0100

    Scale animal postition by viewport dimensions
    This is due to animal positions being in the range [0, 1].

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import * as sim from "lib-simulation-wasm";
 
 const simulation = new sim.Simulation();
 const viewport = document.getElementById('viewport');
+const viewportWidth = viewport.width;
+const viewportHeight = viewport.height;
+
 const ctxt = viewport.getContext('2d');
 
 ctxt.fillStyle = 'rgb(0, 0, 0)';
 
 for (const animal of simulation.world().animals) {
-    ctxt.fillRect(animal.x, animal.y, 15, 15);
+    ctxt.fillRect(
+        animal.x * viewportWidth,
+        animal.y * viewportHeight,
+        15,
+        15,
+    );
 }
 
