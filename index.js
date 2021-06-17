651da43d4f9527f2e04203230940395630c73536
Benjamin Doerry <ben.doerry@gmail.com>
2021-06-17 22:18:39 +0100

    Experiment with `fillStyle` and `fillRect`

--- index.js
+++ index.js
@@ -4,8 +4,38 @@ const simulation = new sim.Simulation();
 const world = simulation.world();
 
 const viewport = document.getElementById('viewport');
-// ------------- ^------^
-// | `document` is a global object that allows to access and modify
-// | current page (e.g. create or remove stuff from it).
+const ctxt = viewport.getContext('2d');
+
+// ---
+// | Determines color of the upcoming shape.
+// - v-------v
+ctxt.fillStyle = 'rgb(255, 0, 0)';
+// ------------------ ^-^ -^ -^
+// | Each of those three parameters is a number from range 0 up to 255:
+// |
+// | rgb(0, 0, 0) = black
+// |
+// | rgb(255, 0, 0) = red
+// | rgb(0, 255, 0) = green
+// | rgb(0, 0, 255) = blue
+// |
+// | rgb(255, 255, 0) = yellow
+// | rgb(0, 255, 255) = cyan
+// | rgb(255, 0, 255) = magenta
+// |
+// | rgb(128, 128, 128) = gray
+// | rgb(255, 255, 255) = white
+// ---
+
+ctxt.fillRect(10, 10, 100, 50);
+// ---------- X   Y   W    H
+// | Draws rectangle filled with color determined by `fillStyle`.
+// |
+// | X = position on the X axis (left-to-right)
+// | Y = position on the Y axis (top-to-bottom)
+// | W = width
+// | X = height
+// |
+// | (unit: pixels)
 // ---
 
