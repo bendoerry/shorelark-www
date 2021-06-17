cea1673da8ade2e5510946001e9ba589fe77786d
Benjamin Doerry <ben.doerry@gmail.com>
2021-06-17 22:16:56 +0100

    Create `canvas` on page

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import * as sim from "lib-simulation-wasm";
 
 const simulation = new sim.Simulation();
 const world = simulation.world();
-// --------------------- ^---^
-// | Parsing already happens inside this automatically-generated
-// | function - we don't have to do anything more in here.
+
+const viewport = document.getElementById('viewport');
+// ------------- ^------^
+// | `document` is a global object that allows to access and modify
+// | current page (e.g. create or remove stuff from it).
 // ---
 
