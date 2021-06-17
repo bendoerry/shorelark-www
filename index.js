b87727e54b1c8206912c0ec718350e7fb568c55a
Benjamin Doerry <ben.doerry@gmail.com>
2021-06-17 22:22:11 +0100

    Try drawing animals as squares on canvas

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import * as sim from "lib-simulation-wasm";
 
 const simulation = new sim.Simulation();
-const world = simulation.world();
-
 const viewport = document.getElementById('viewport');
 const ctxt = viewport.getContext('2d');
+
 ctxt.fillStyle = 'rgb(0, 0, 0)';
+
+for (const animal of simulation.world().animals) {
+    ctxt.fillRect(animal.x, animal.y, 15, 15);
+}
 
